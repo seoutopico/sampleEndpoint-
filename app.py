8d7a3e004febbbb1ea54faa3ba7e9b1af40a4b74
Aina Lluna <aina.lluna@gmail.com>
2024-09-14 10:22:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-#Set this as an API endpoint via FastAPI
 
 app = FastAPI()
 
-class Query(BaseModel):
-    texto: str
+@app.get("/saludo")
+async def saludo(query: str):
+    return {"mensaje": f"Hola, {query}"}
 
-@app.post("/")
-def insertar_query(query: Query):
-    return {"frase_resultante": f"Hola, {query.texto}"}
